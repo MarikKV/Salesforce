739b0026c5f4b93f8ab519e83a835932a5014eae
Marik1 <kosaniakm@gmail.com>
2020-01-28 01:47:37 +0200

    add cart button delete

--- force-app/main/default/aura/cartItemComponent/cartItemComponentController.js
+++ force-app/main/default/aura/cartItemComponent/cartItemComponentController.js
@@ -1,5 +1,16 @@
 ({
-    myAction : function(component, event, helper) {
+	delProductFromCart : function(component, event, helper) {
+        //var category = event.getSource().get('v.label');
 
-    }
+        var product = event.getSource().get('v.name');
+        console.log('Push id parent ', product);
+        //var prodId = event.getSource().get('v.category__c');
+
+        //items.push(product);
+        var appEvent = $A.get("e.c:delProductFromCartEvent");
+        appEvent.setParams({"product" : product});
+        appEvent.fire();
+
+        //console.log('add', product, 'to cart');
+	}
 })
--- force-app/main/default/aura/shopComponent/shopComponentController.js
+++ force-app/main/default/aura/shopComponent/shopComponentController.js
@@ -40,5 +40,25 @@
         productsInCart.push(product);
         console.log('after push', productsInCart);
         cmp.set("v.product", productsInCart);
+    },
+    handleDelProductFromCartEvent : function(cmp, event) {
+        var product = event.getParam("product");
+        var productsInCart = cmp.get("v.product");
+        console.log('Id', product, 'arr', productsInCart, 'length ', productsInCart.length);
+        for(let i = 0; i < productsInCart.length; i++){
+            if(productsInCart[i].Id === product){
+                console.log(i, productsInCart[i].name__c);
+                productsInCart.splice(i, 1);
+            }else{
+                console.log('pff');
+            } 
+        }
+        cmp.set("v.product", productsInCart);
+        /*console.log(productsInCart[0].name__c, productsInCart[1].name__c, productsInCart[2].name__c);
+        console.log('all in cart', newList, 'all');
+        console.log('before push', cartProducts, 'all');
+        productsInCart.push(product);
+        console.log('after push', productsInCart);
+        cmp.set("v.product", productsInCart);*/
     }
 })
